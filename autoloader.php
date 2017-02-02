6b17fbbd1859f95d37f7ecc07f724f9b6af1828b
Kirlat <kirlat@yula.media>
2017-02-02 14:04:22 +0400

    Spaces converted to tabs to match WP coding standards

--- autoloader.php
+++ autoloader.php
@@ -9,95 +9,95 @@ namespace Blaze\API;
  */
 class Autoloader
 {
-    protected static $instance;
+	protected static $instance;
 
-    private $blazePrefix;
-    private $baseDir;
+	private $blazePrefix;
+	private $baseDir;
 
-    /**
-     * Autoloader constructor.
-     *
-     * @param string $baseDir Blaze Core base directory (default: dirname(__FILE__))
-     */
-    private function __construct($baseDir = null)
-    {
-        $this->blazePrefix = 'Blaze\\API\\';
+	/**
+	 * Autoloader constructor.
+	 *
+	 * @param string $baseDir Blaze Core base directory (default: dirname(__FILE__))
+	 */
+	private function __construct($baseDir = null)
+	{
+		$this->blazePrefix = 'Blaze\\API\\';
 
-        if ($baseDir === null) {
-            $baseDir = dirname(__FILE__);
-        }
+		if ($baseDir === null) {
+			$baseDir = dirname(__FILE__);
+		}
 
-        // realpath doesn't always work, for example, with stream URIs
-        $realDir = realpath($baseDir);
-        if (is_dir($realDir)) {
-            $this->baseDir = $realDir;
-        } else {
-            $this->baseDir = $baseDir;
-        }
-    }
+		// realpath doesn't always work, for example, with stream URIs
+		$realDir = realpath($baseDir);
+		if (is_dir($realDir)) {
+			$this->baseDir = $realDir;
+		} else {
+			$this->baseDir = $baseDir;
+		}
+	}
 
-    /**
-     * Private clone method to prevent cloning of the instance of the
-     * WpSite instance.
-     *
-     * @return void
-     */
-    private function __clone()
-    {
-    }
+	/**
+	 * Private clone method to prevent cloning of the instance of the
+	 * WpSite instance.
+	 *
+	 * @return void
+	 */
+	private function __clone()
+	{
+	}
 
-    /**
-     * Private unserialize method to prevent unserializing of the WpSite
-     * instance.
-     *
-     * @return void
-     */
-    private function __wakeup()
-    {
-    }
+	/**
+	 * Private unserialize method to prevent unserializing of the WpSite
+	 * instance.
+	 *
+	 * @return void
+	 */
+	private function __wakeup()
+	{
+	}
 
-    /**
-     * Register a new instance as an SPL autoloader.
-     *
-     * @param string $baseDir Blaze Core base directory (default: dirname(__FILE__))
-     *
-     * @return Autoloader Registered Autoloader instance
-     */
-    public static function register($baseDir = null)
-    {
-        if (is_null(self::$instance)) {
-            self::$instance = new self($baseDir);
-            spl_autoload_register(array(self::$instance, 'autoload'));
-        }
-        return self::$instance;
-    }
+	/**
+	 * Register a new instance as an SPL autoloader.
+	 *
+	 * @param string $baseDir Blaze Core base directory (default: dirname(__FILE__))
+	 *
+	 * @return Autoloader Registered Autoloader instance
+	 */
+	public static function register($baseDir = null)
+	{
+		if (is_null(self::$instance)) {
+			self::$instance = new self($baseDir);
+			spl_autoload_register(array(self::$instance, 'autoload'));
+		}
+		return self::$instance;
+	}
 
-    /**
-     * Autoload Blaze API classes.
-     *
-     * @param string $class
-     */
-    public function autoload($class)
-    {
-        if ($class[0] === '\\') {
-            $class = substr($class, 1);
-        }
+	/**
+	 * Autoload Blaze API classes.
+	 *
+	 * @param string $class
+	 */
+	public function autoload($class)
+	{
+		if ($class[0] === '\\') {
+			$class = substr($class, 1);
+		}
 
-        // Handle only Blaze subnamespaces
-        if (strpos($class, $this->blazePrefix) !== 0) {
-            return;
-        }
+		// Handle only Blaze subnamespaces
+		if (strpos($class, $this->blazePrefix) !== 0) {
+			return;
+		}
 
-        $name = substr($class, strlen($this->blazePrefix));
-        $name = explode('\\', str_replace('_', '-', strtolower($name)));
-        $name[sizeof($name) - 1] = 'class-' . $name[sizeof($name)-1];
-        $name = implode(DIRECTORY_SEPARATOR, $name);
+		$name = substr($class, strlen($this->blazePrefix));
+		$name = explode('\\', str_replace('_', '-', strtolower($name)));
+		$name[sizeof($name) - 1] = 'class-' . $name[sizeof($name)-1];
+		$name = implode(DIRECTORY_SEPARATOR, $name);
 
-        $file = sprintf('%s' . DIRECTORY_SEPARATOR .'%s.php', $this->baseDir, $name);
-        if (is_file($file)) {
-            require $file;
-        }
-    }
+		$file = sprintf('%s' . DIRECTORY_SEPARATOR .'%s.php', $this->baseDir, $name);
+		if (is_file($file)) {
+			require $file;
+		}
+	}
 }
 
 Autoloader::register();
--- routes/class-list-controller.php
+++ routes/class-list-controller.php
@@ -4,63 +4,63 @@ namespace Blaze\API\Routes;
 
 class List_Controller extends \WP_REST_Controller
 {
-    /**
-     * Register the routes for the objects of the controller.
-     */
-    public function register_routes() {
-        $version = '1';
-        $namespace = 'blaze/v' . $version;
-        $base = 'list';
-        register_rest_route( $namespace, '/' . $base, array(
-            array(
-                'methods'         => \WP_REST_Server::READABLE,
-                'callback'        => array( $this, 'get_items' ),
-                'args'            => array(
+	/**
+	 * Register the routes for the objects of the controller.
+	 */
+	public function register_routes() {
+		$version = '1';
+		$namespace = 'blaze/v' . $version;
+		$base = 'list';
+		register_rest_route( $namespace, '/' . $base, array(
+			array(
+				'methods'         => \WP_REST_Server::READABLE,
+				'callback'        => array( $this, 'get_items' ),
+				'args'            => array(
 
-                ),
-            )
-        ) );
-    }
+				),
+			)
+		) );
+	}
 
-    /**
-     * Get a collection of items
-     *
-     * @param \WP_REST_Request $request Full data about the request.
-     * @return \WP_Error|\WP_REST_Response
-     */
-    public function get_items( $request ) {
-        // Query parameters
-        $query_params = array(
-            'post_type' => 'post',
-            'posts_per_page' => -1
-        );
+	/**
+	 * Get a collection of items
+	 *
+	 * @param \WP_REST_Request $request Full data about the request.
+	 * @return \WP_Error|\WP_REST_Response
+	 */
+	public function get_items( $request ) {
+		// Query parameters
+		$query_params = array(
+			'post_type' => 'post',
+			'posts_per_page' => -1
+		);
 
-        //get parameters from request
-        $params = $request->get_params();
+		//get parameters from request
+		$params = $request->get_params();
 
-        if (isset($params['category_name']) || array_key_exists('category_name', $params)) {
-            $categories = implode(',', $params['category_name']);
-            $query_params['category_name'] = $categories;
-        }
+		if (isset($params['category_name']) || array_key_exists('category_name', $params)) {
+			$categories = implode(',', $params['category_name']);
+			$query_params['category_name'] = $categories;
+		}
 
-        $query = new \WP_Query( $query_params );
-        $items = array();
-        // Check that we have query results.
-        if ( $query->have_posts() ) {
-            // Start looping over the query results.
-            while ( $query->have_posts() ) {
-                $query->the_post();
-                $post_data = array (
-                    'id' => get_the_ID(),
-                    'title' => get_the_title(),
-                    'url' => get_permalink()
-                );
-                array_push($items, $post_data);
-            }
-        }
-        // Restore original post data.
-        wp_reset_postdata();
+		$query = new \WP_Query( $query_params );
+		$items = array();
+		// Check that we have query results.
+		if ( $query->have_posts() ) {
+			// Start looping over the query results.
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				$post_data = array (
+					'id' => get_the_ID(),
+					'title' => get_the_title(),
+					'url' => get_permalink()
+				);
+				array_push($items, $post_data);
+			}
+		}
+		// Restore original post data.
+		wp_reset_postdata();
 
-        return new \WP_REST_Response( $items, 200 );
-    }
+		return new \WP_REST_Response( $items, 200 );
+	}
 }
